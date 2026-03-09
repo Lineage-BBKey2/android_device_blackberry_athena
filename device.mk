c3850d24435b3e0e6fd96def8f3f966453ba94d1
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-03-09 20:21:00 +0100

    athena: device.mk: Sort and re-organize
    Change-Id: I2ca39ca708941295647da0c599005a88ab36fba0

--- device.mk
+++ device.mk
@@ -15,6 +15,9 @@
 
 DEVICE_PATH := device/blackberry/athena
 
+# Vendor blobs
+$(call inherit-product, vendor/blackberry/athena/athena-vendor.mk)
+
 # Set Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 27
 
@@ -22,36 +25,33 @@ PRODUCT_SHIPPING_API_LEVEL := 27
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
-
-# Get non-open-source specific aspects
-$(call inherit-product, vendor/blackberry/athena/athena-vendor.mk)
-
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Audio configs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 1620
 TARGET_SCREEN_WIDTH := 1080
 
-# Athena audio configs
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+# Camera shims
+PRODUCT_PACKAGES += \
+    libskia_shim \
+    libjnigraphics_shim
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-blackberry
 
-# Goodix - libbinder shim
+# Goodix shims
 PRODUCT_PACKAGES += \
     libbinder_shim.vendor \
     libfakelogprint
-
-# Camera - Shims for OREO camera HAL
-PRODUCT_PACKAGES += \
-    libskia_shim \
-    libjnigraphics_shim
 
 # Touch Keypad
 PRODUCT_PACKAGES += \
